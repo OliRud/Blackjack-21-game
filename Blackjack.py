372014a458efcaabee61ce34c7f9c0cb7e18023f
OliRud <160233200@student.rnngroup.ac.uk>
2024-06-09 22:57:12 +0100

    finishing off

--- Blackjack.py
+++ Blackjack.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 suits = ["spades","clubs","diamonds","hearts"]
 ranks = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
@@ -27,7 +28,7 @@ class Deck:
         random.shuffle(self.deck)
 
 
-    def show_deck(self):
+    def show_deck(self): #for debug(not used)
         print(self.deck)
     
 
@@ -48,13 +49,18 @@ class Chips:
         self.chips -= bet
 
     def win(self):
-        pass
+        self.chips += (self.bet * 2)
+        print("\nYOU WIN",str(self.bet),"CHIPS")
+        self.bet = 0
 
     def lose(self):
-        pass
+        print("\nYOU LOSE")
+        self.bet = 0
 
     def push(self):
-        pass
+        self.chips += self.bet
+        print("\nPUSH")
+        self.bet = 0
 
 
 
@@ -63,17 +69,26 @@ class Hand:
     def __init__(self):
         self.hand = []
         self.value = 0
+        self.bust = False
     
     def hit(self,Deck):
         card = Deck.deal_card()
+
+        if card[1] == 11 and self.value > 11: #convert aces to 1 if the value of the hand is over 11
+            card[1] = 1
+
         self.hand.append(card[0])
         self.value += card[1]
+
+        if self.value > 21:
+            self.bust = True
 
         return self.hand,self.value
     
     def empty(self):
         self.hand = []
         self.value = 0
+        self.bust = False
 
 
 
@@ -81,7 +96,7 @@ class Hand:
 
 #functions
 
-def bet(Chips): #processing the bet
+def bet(): #processing the bet
 
     while True:
         try:
@@ -98,11 +113,25 @@ def bet(Chips): #processing the bet
             print("\nINVALID INPUT, TRY AGAIN.")
 
 
-def hit():
-    pass
+def endgame(Dealer_hand,Player_hand): #calculates the winner
+
+    if Player_hand.bust == True:
+        Chips.lose()
+    elif Dealer_hand.bust == True:
+        Chips.win()
+    elif Dealer_hand.value > Player_hand.value:
+        Chips.lose()
+    elif Dealer_hand.value < Player_hand.value:
+        Chips.win()
+    elif Dealer_hand.Value == Player_hand.value:
+        Chips.push()
+    
+    print("\nCurrent Chips:",Chips.chips)
+    input("\nPress enter to play again")
+    
 
 
-def table(Dealer_hand,Player_Hand): #table is the main game 
+def table(Dealer_hand,Player_hand,Deck): #table is the main game 
 
     global turn
 
@@ -112,14 +141,21 @@ def table(Dealer_hand,Player_Hand): #table is the main game
 
         #the player always goes first, once their turn is over it becomes the dealer's turn, then the game ends and the rewards are given out
         if turn == "player":
-            print("\nDealer's hand:",str(Dealer_hand.hand[0])+" -Card Hidden-\n\nPlayer's Hand:",str(Player_Hand.hand)) #dealers second hand is hidden
+            print("\nDealer's hand:",str(Dealer_hand.hand[0]),"-Card Hidden-\n\nPlayer's Hand:",str(Player_hand.hand),str(Player_hand.value)) #dealers second hand is hidden
 
             try:
-                action = int(input("\nEnter action (Enter 1 or 2):\n(1)Hit\n(2)Stand"))
+                action = int(input("\nEnter action (Enter 1 or 2):\n(1)Hit\n(2)Stand\n"))
 
                 #player decision
-                if input == 1:#hit
-                    hit()
+                if action == 1:#hit
+
+                    print("\n-You Hit-\n")
+                    
+                    Player_hand.hit(Deck)
+
+                    if Player_hand.bust == True:
+                        print("\n-PLAYER BUST-\n")
+                        turn = "dealer"
 
                 else:#stand
                     turn = "dealer"
@@ -127,40 +163,58 @@ def table(Dealer_hand,Player_Hand): #table is the main game
             except:
                 print("Invalid input")
 
-        else:
-            print("\nDealer's hand:",str(Dealer_hand.hand)+"\n\nPlayer's Hand:",str(Player_Hand.hand))
+        #dealer
+        elif turn == "dealer":
+
+            print("\n-Dealer's turn-\n")
+
+            print("\nDealer's hand:",str(Dealer_hand.hand),str(Dealer_hand.value)+"\n\nPlayer's Hand:",str(Player_hand.hand))
+
+            time.sleep(1)
+
+            if Player_hand.bust == False and Dealer_hand.value < 16:
+
+                Dealer_hand.hit(Deck)
+                time.sleep(1)
+            
+            else:
+                
+                endgame(Dealer_hand,Player_hand)
+                break
 
 
 turn = "player"
 
-#main
 Deck = Deck(suits,ranks,values)
 
 Chips = Chips()
 
 Dealer_hand = Hand()
 
-Player_Hand = Hand()
+Player_hand = Hand()
 
 print("--WELCOME TO BLACKJACK--")
+print("\nChips:",Chips.chips)
 
+#main loop
 while True:
 
-    Deck.shuffle()
+    Deck.__init__(suits,ranks,values) #replenish the deck every new game
 
-    print("\nChips:",Chips.chips)
+    Player_hand.empty() #empty the hands every new game
+    Dealer_hand.empty()
+
+    Deck.shuffle() #shuffle the deck every new game
 
     #get the bet and deduct the bet from the chips
-    bet = bet(Chips)
+    bet = bet()
     Chips.take_bet(bet)
 
     #add 2 cards automatically to both hands
     Dealer_hand.hit(Deck)
     Dealer_hand.hit(Deck)
 
-    Player_Hand.hit(Deck)
-    Player_Hand.hit(Deck)
+    Player_hand.hit(Deck)
+    Player_hand.hit(Deck)
 
-    table(Dealer_hand,Player_Hand)
-
-    break
+    table(Dealer_hand,Player_hand,Deck)
